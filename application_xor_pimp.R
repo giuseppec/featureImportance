10eb1ee8f09d795f5c6e3c822b80798c5b50e312
giuseppec <giuseppe.casalicchio@gmail.com>
2018-01-10 10:00:26 +0100

    update code

--- application_xor_pimp.R
+++ application_xor_pimp.R
@@ -3,43 +3,6 @@ library(BBmisc)
 library(mlr)
 load_all("pkg")
 
-set.seed(1)
-learner = makeLearner("classif.randomForest", predict.type = "prob")
-learner = setHyperPars(learner, importance = TRUE, ntree = 20)
-task = convertMLBenchObjToTask("mlbench.xor", n = 1000, d = 2)
-
-# add correlated feature
-d = getTaskData(task)
-d$x.3 = d$x.1 + rnorm(getTaskSize(task), sd = 0.25)
-
-# add n additional noisy features
-n = 2
-dim = ncol(d) - 1
-X.noisy = setColNames(replicate(n = 2, runif(getTaskSize(task))), paste0("x.", dim + 1:n))
-d = cbind(d, X.noisy)
-task = makeClassifTask(data = d, target = "classes") # pid.task
-
-resampling = makeResampleInstance(makeResampleDesc("CV", iter = 3, stratify = TRUE), task)
-
-library(parallelMap)
-parallelStartSocket(30)
-#parallelExport("permuteFeature", "featureImportance", "performanceDrop")
-parallelLibrary("checkmate", "BBmisc", "mlr", "featureImportance")
-feat.names = getTaskFeatureNames(task)
-feat.imp = featureImportance(learner, task, resampling, measures, n.feat.perm = 1000)
-imp = feat.imp$importance[, lapply(.SD, mean), .SDcols = "acc", by = c("features")]
-imp$lo = feat.imp$importance[, lapply(.SD, quantile, c(0.05)), .SDcols = "acc", by = c("features")]$acc
-imp$up = feat.imp$importance[, lapply(.SD, quantile, c(0.95)), .SDcols = "acc", by = c("features")]$acc
-imp
-
-null = nullImportance(learner, task, resampling, measures, n.feat.perm = 10, n.target.perm = 100)
-null2 = null[, lapply(.SD, mean), .SDcols = "acc", by = c("features", "n.feat.perm", "n.target.perm")]
-#null3 = null[, lapply(.SD, mean), .SDcols = "acc", by = c("features", "n.target.perm")]
-
-null4 = nullImportance(learner, task, resampling, measures, n.feat.perm = 1, n.target.perm = 1000)
-null5 = null4[, lapply(.SD, mean), .SDcols = "acc", by = c("features", "n.feat.perm", "n.target.perm")]
-parallelStop()
-
 pvalue = function(null, imp, measure.id = "acc") {
   null = as.data.frame(null)
   imp = as.data.frame(imp)
@@ -58,33 +21,91 @@ pvalue = function(null, imp, measure.id = "acc") {
   return(ret)
 }
 
-p = pvalue(null5, imp)
-sort(vnapply(p, function(x) x$p.value))
+set.seed(1)
+learner = makeLearner("classif.randomForest", predict.type = "prob")
+learner = setHyperPars(learner, importance = TRUE, ntree = 20)
+task = convertMLBenchObjToTask("mlbench.xor", n = 1000, d = 2)
+
+# add correlated feature
+d = getTaskData(task)
+d$x.3 = d$x.1 + rnorm(getTaskSize(task), sd = 0.25)
+
+# add n additional noisy features
+n = 2
+dim = ncol(d) - 1
+X.noisy = setColNames(replicate(n = 2, runif(getTaskSize(task))), paste0("x.", dim + 1:n))
+task = makeClassifTask(data = cbind(d, X.noisy), target = "classes")
+
+#
+library(OpenML)
+#task = pid.task
+task = convertOMLDataSetToMlr(getOMLDataSet(150))
+
+d = getTaskData(task)
+#resampling = makeResampleInstance(makeResampleDesc("CV", iter = 10, stratify = TRUE), task)
+resampling = makeResampleInstance(makeResampleDesc("RepCV", fold = 5, rep = 5, stratify = TRUE), task)
+
+library(parallelMap)
+parallelStartSocket(30)
+#parallelExport("permuteFeature", "featureImportance", "performanceDrop")
+parallelLibrary("checkmate", "BBmisc", "mlr", "featureImportance")
+feat.names = getTaskFeatureNames(task)
+feat.imp = featureImportance(learner, task, resampling, measures, n.feat.perm = 100)
+imp = feat.imp$importance[, lapply(.SD, mean), .SDcols = "acc", by = c("features")]
+#imp$lo = feat.imp$importance[, lapply(.SD, quantile, c(0.05)), .SDcols = "acc", by = c("features")]$acc
+#imp$up = feat.imp$importance[, lapply(.SD, quantile, c(0.95)), .SDcols = "acc", by = c("features")]$acc
+imp
+#null = nullImportance(learner, task, resampling, measures, n.feat.perm = 10, n.target.perm = 100)
+#null2 = null[, lapply(.SD, mean), .SDcols = "acc", by = c("features", "n.feat.perm", "n.target.perm")]
+#null3 = null[, lapply(.SD, mean), .SDcols = "acc", by = c("features", "n.target.perm")]
+null = nullImportance(learner, task, resampling, measures, n.feat.perm = 1, n.target.perm = 100)
+null2 = null[, lapply(.SD, mean), .SDcols = "acc", by = c("features", "n.feat.perm", "n.target.perm")]
+parallelStop()
+
+p = pvalue(null2, imp, measure.id = "acc")
+(p = sort(vnapply(p, function(x) x$p.value)))
 
 i = 3
 plot(density(p[[i]]$null.dist), main = names(p)[i])
 abline(v = p[[i]]$true, col = "red")
 
+# Importance using RF internal's feature importance
+mod = train(learner, task)
+#imp.intern = getFeatureImportance(mod, "permutation", type = 1)$res
+#imp.intern = melt(imp.intern, measure.vars = getTaskFeatureNames(task), variable.name = "features")
+imp.intern = setColNames(as.data.frame(importance(getLearnerModel(mod), type = 1, scale = FALSE)), "value")
+imp.intern$features = row.names(imp.intern)
 
+null.intern = nullImportanceRF(learner, task, n.target.perm = 100)
+null.intern2 = melt(null.intern, measure.vars = getTaskFeatureNames(task), variable.name = "features")
+null.intern2
 
+p.intern = pvalue(null.intern2, imp.intern, measure.id = "value")
+(p.intern = sort(vnapply(p.intern, function(x) x$p.value)))
+
+# Importance using vita
 library(vita)
 mod = train(learner, task)
-d = getTaskData(task)
 target = getTaskTargetNames(task)
-imp = getFeatureImportance(mod, "permutation")
-imp
+d = getTaskData(task)
+features = getTaskFeatureNames(task)
 
-pimp = vita:::PIMP.default(d[, !colnames(d) %in% target], d[, target], mod$learner.model, S = 1000)
+# res = resample(learner, task, resampling, measures, models = TRUE)
+# imp.vita = performanceDrop(object = res, data = d, features = features, measures = measures)
+# imp.vita = imp.vita[, lapply(.SD, mean), .SDcols = "acc", by = c("features")]
+# imp.vita = sortByCol(imp.vita, "features")
+# merge(imp.vita, imp.intern)
+
+pimp = vita:::PIMP.default(d[, !colnames(d) %in% target], d[, target], mod$learner.model, S = 100)
 sort(pimp$VarImp[, "VarImp"])
-test = vita:::PimpTest.default(pimp)
-test = cbind(test$VarImp, test$pvalue)
-test[order(test[, "p-value"], decreasing = TRUE),]
+imp.vita = vita:::PimpTest.default(pimp)
+imp.vita = cbind(imp.vita$VarImp, imp.vita$pvalue)
+imp.vita[order(imp.vita[, "p-value"], decreasing = TRUE),]
 
-
-res = resample(learner, task, resampling, measures, models = TRUE)
-
-imp = performanceDrop(object = res, data = d, features = features, measures = measures)
-imp[, lapply(.SD, mean), .SDcols = "acc", by = c("features")]
+compare = as.data.frame(imp.vita[order(imp.vita[, "p-value"], decreasing = TRUE),])
+compare$imp = p[row.names(compare)]
+compare$imp.intern = p.intern[row.names(compare)]
+compare
 
 #parallelStartSocket(30)
 shapley = shapleyImportance(object = res, data = d, target = target, features = features,
