8dfc72e509b0fa22da4701a2954e3758c1cee62b
giuseppec <giuseppe.casalicchio@gmail.com>
2018-04-16 16:58:11 +0200

    make code pretty

--- ecml-demo/application_shapley_simulation.R
+++ ecml-demo/application_shapley_simulation.R
@@ -10,41 +10,28 @@ library(stringi)
 library(MASS)
 library(featureImportance)
 library(batchtools)
-#load_all("../featureImportance")
 
-# create registry
-id = "application_shapley_2_of_3_interact_sd_0.5"
-model.file = paste0("paper/application/", id, "_models.Rds")
-path = paste0("paper/application/", id)
+# create batchtools registry
+path = "application_shapley_simulation"
 unlink(path, recursive = TRUE)
 reg = makeExperimentRegistry(
   file.dir = path,
-  packages = c("mlr", "BBmisc", "parallelMap", "featureImportance", "MASS", "checkmate", "stringi"),
-  source = "paper/application/helper_functions.R",
+  packages = c("mlr", "BBmisc", "featureImportance", "MASS", "checkmate", "stringi"),
+  source = "helper_functions.R",
   seed = 123)
+# uncomment this line to run experiments in parallel
 reg$cluster.functions = makeClusterFunctionsSocket(30)
 
 # specify data
 set.seed(1)
-d = diag(3)
-addCorr = function(d, cor = 0) {
-  replace(d, d == 0, cor)
-}
-sig = list(
-  cor00 = addCorr(d, 0)#,
-  #cor40 = addCorr(d, 0.4),
-  #cor50 = addCorr(d, 0.5)
-)
+sig = diag(3)
 n = 10000
 generateY = function(X) {
   rowSums(model.matrix(~ . + V1:V2 - 1, data = X)) + rnorm(nrow(X), sd = 0.5)
-  #rowSums(X) + rnorm(nrow(X), sd = 0.1)
 }
-tasks = lapply(sig, function(x) {
-  X = as.data.frame(mvrnorm(n, mu = rep(0, ncol(x)), Sigma = x))
-  X$y = generateY(X)
-  makeRegrTask(data = X, target = "y")
-})
+X = as.data.frame(mvrnorm(n, mu = rep(0, ncol(sig)), Sigma = sig))
+X$y = generateY(X)
+task = makeRegrTask(data = X, target = "y")
 
 # create learners
 lrns = list(
@@ -54,29 +41,13 @@ lrns = list(
   makeLearner("regr.randomForest", ntree = 100, importance = TRUE)
 )
 
-# make/load models
-mod = vector("list", length(lrns)*length(tasks))
-
 # add problems
-ind = 1
-pid = character(length(lrns)*length(tasks))
-for (lrn in lrns) {
-  for (task.name in names(tasks)) {
-    # create model if not already stored
-    mod[[ind]] = train(lrn, tasks[[task.name]])
-    addProblem(name =  paste0(getLearnerId(lrn), task.name), data = list(
-      mod = mod[[ind]],
-      sigma = sig[[task.name]],
-      n = 100,
-      generateY = generateY,
-      measures = list(mse, mae, medae, rmse, medse)
-    ), seed = ind)
-    # increment counter
-    ind = ind + 1
-  }
+for (i in seq_along(lrns)) {
+  lrn = lrns[[i]]
+  mod = train(lrn, task)
+  prob.pars = list(mod = mod, sigma = sig, n = 100, generateY = generateY, measures = mlr::mse)
+  addProblem(name = getLearnerId(lrn), data = prob.pars, seed = i)
 }
-# save models
-saveRDS(mod, file = model.file)
 
 # add algorithms
 addAlgorithm("pfi", fun = function(job, instance, data) {
@@ -125,21 +96,18 @@ addAlgorithm("ge", fun = function(job, instance, data) {
   list(res = ge(mod, data = X, target, measures, feat), data = X)
 })
 
-#addExperiments(repls = 100)
-addExperiments(repls = 100)
+addExperiments(repls = 500)
 submitJobs(ids = findNotSubmitted(), reg = reg)
 
 # get results
 res = reduceResultsList(findDone(), fun = function(x, job) {
   x$res
 })
+
 job = getJobTable()
 repl = job$repl
-cor = as.numeric(gsub(".*cor", "", job$problem))/100
-learner = gsub("cor.*", "", job$problem)
-measures = list(mse, mae, medae, rmse, medse)
-mid = vcapply(measures, function(x) x$id)
-cols = c("method", "feature", mid)
+learner = job$problem
+cols = c("method", "feature", "mse")
 res.cleanup = lapply(res, function(x) {
   if ("V1" %in% names(x))
     res = rbindlist(lapply(x, rbindlist, idcol = "method"), idcol = "feature") else
@@ -150,29 +118,22 @@ res.cleanup = lapply(res, function(x) {
   setcolorder(res, cols)
 })
 res.cleanup = lapply(1:length(res.cleanup), function(i) {
-  cbind(res.cleanup[[i]], learner = learner[i], cor = cor[i], repl = repl[i])
+  cbind(res.cleanup[[i]], learner = learner[i], repl = repl[i])
   })
 res.cleanup = rbindlist(res.cleanup)
-saveRDS(res.cleanup, file = paste0("paper/application/", id, ".Rds"))
-
-
+saveRDS(res.cleanup, file = paste0(path, ".Rds"))
 
 ### Plot results
-#res.cleanup = readRDS("paper/application/application_shapley_2_of_3_interact.Rds")
-(res.perf = subset(res.cleanup, method %in% c("ge0", "geP")))
 res.cleanup = subset(res.cleanup, method %in% c("pfi.diff", "pfi.ratio", "shapley"))
-
 # compute percentages for 3 features
-res.cleanup[, perc := mse/mse[feature == "V3"], #100*mse/sum(mse),
-  by = c("method", "learner", "repl", "cor")] #perc := .SD/sum(.SD), .SDcols = c("mse"),
-# res.cleanup[, perc.mae := ifelse(feature == "V3", abs(perc - 25), abs(perc - 37.5)) ]
+res.cleanup[, ratio := mse/mse[feature == "V3"], by = c("method", "learner", "repl")]
+res.cleanup = subset(res.cleanup, feature != "V3")
 
-lab.cor = function(cor) paste("Cor.", cor)
 lab.lrn = function(lrn) gsub("regr.", "", lrn)
-pp = ggplot(data = res.cleanup, aes(x = feature, y = perc))
+pp = ggplot(data = res.cleanup, aes(x = feature, y = ratio))
 pp = pp + geom_boxplot(aes(fill = method), lwd = 0.25, outlier.size = 0.75)
-pp = pp + facet_grid(cor ~ learner, scales = "free",
-  labeller = labeller(cor = lab.cor, learner = lab.lrn))
+pp = pp + facet_grid(. ~ learner, scales = "free",
+  labeller = labeller(learner = lab.lrn))
 # = pp + geom_hline(yintercept = c(100/3)) #+ geom_hline(yintercept = c(25))
 pp = pp +
   scale_fill_discrete(name = "method",  labels = c("PFI (Diff.)", "PFI (Ratio)", "SFIMP"))  +
